5892e50f7e1a05ce6043eb4fbbee864c13e43ba6
rachfuerst <rach.fuerst@gmail.com>
2020-08-30 12:58:36 -0500

    Styling text in Google infoWindow

--- src/components/restaurants/GoogleMapWithMarker.jsx
+++ src/components/restaurants/GoogleMapWithMarker.jsx
@@ -2,6 +2,7 @@
 
 // Import React
 import * as React from 'react'
+import styled from '@emotion/styled'
 
 // Import necessary components for React Google Maps
 import {
@@ -15,10 +16,51 @@ import {
 // Import custom styles to customize the style of Google Map
 const styles = require('./GoogleMapStyles.json')
 
+const Container = styled.div`
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 2px 7px 1px rgba(0, 0, 0, 0.3);
+  font-size: 13px;
+  font-weight: 300;
+  padding: 12px;
+`
+const Header = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  margin: 0.5em;
+`
+
+const Information = styled.div`
+  text-align: center;
+  margin: 0.5em;
+`
+
+const Yelp = styled.a`
+  color: #3088ff !important;
+  text-decoration: none;
+  text-align: center !important;
+
+
+  &:visited {
+    text-decoration: none;
+    color: green !important;
+    text-align: center !important;
+  }
+  &:hover {
+    color: red !important;
+    text-align: center !important;
+  }
+  &:link {
+    color: #3088ff !important;
+  text-decoration: none;
+  text-align: center !important;
+  }
+`
+
 // Import custom icon for map marker
 // You can use this if you need to support IE11 and lower.
 // const mapMarker = require('./GoogleMapMarker.svg')
-
 // Google Map component
 const GoogleMapWithMarker = withScriptjs(
   withGoogleMap(props => (
@@ -71,19 +113,13 @@ const GoogleMapWithMarker = withScriptjs(
           }}
           onCloseClick={() => props.handleInfoboxClick()}
         >
-          <div>
-            <div>
-              <div>
-
-                <h4>{props.infoboxMessage}</h4>
-                <h4>{props.desc}</h4>
-                <h4>{props.yelp} </h4>
-        
-               
-                
-              </div>
-            </div>
-          </div>
+          <Container>
+            <Header>{props.infoboxMessage}</Header>
+            <Yelp href={props.yelp} target="_blank">
+              Check them out on Yelp
+            </Yelp>
+            <Information>{props.desc}</Information>
+          </Container>
         </InfoWindow>
       )}
     </GoogleMap>
